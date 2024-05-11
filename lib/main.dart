9a7db56464453d2f9a039326ae03556c60bf4a42
Mauricio Medina <mau172001mece@gmail.com>
2024-05-11 13:42:17 -0600

    Update main to create MultiblocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formulario/presentation/blocs/blocs.dart';
 import 'package:formulario/presentation/screens/screens.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CubitsProvider());
+}
+
+class CubitsProvider extends StatelessWidget {
+  const CubitsProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => FormCubit())
+      ], 
+      child: const MyApp()
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
